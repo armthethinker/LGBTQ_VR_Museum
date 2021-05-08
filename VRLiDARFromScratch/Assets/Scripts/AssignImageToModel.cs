d481ef437be2b8f815a008091b1e12d59addabc0
Andrew R McHugh <arm5629@gmail.com>
2021-05-07 19:16:43 -0700

    Created script to apply texture to the model
    That way we have can have one base material in our folders (rather than have to create a new material for each model)

---
+++ VRLiDARFromScratch/Assets/Scripts/AssignImageToModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AssignImageToModel : MonoBehaviour
+{
+    public Texture textureToApply;
+    public GameObject meshObject;
+    void Start()
+    {
+        meshObject.GetComponent<Renderer>().materials[0].SetTexture("_MainTex", textureToApply);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
